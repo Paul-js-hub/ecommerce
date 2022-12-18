f01a628b7e01a3e25642f50e405ce29f87e8d2a9
Paul-js-hub <hesponpoisson@gmail.com>
2022-12-18 11:34:30 +0300

    Add a product details page

--- src/components/ProductDetail/ProductDetail.js
+++ src/components/ProductDetail/ProductDetail.js
@@ -1,7 +1,21 @@
+import { useLocation }from 'react-router-dom'
+import {Button, Col, Row} from 'antd'
 const ProductDetail = () => {
-    return (
-        <h1>Product details page</h1>
-    );
+    const { state } = useLocation();
+    const { product } = state || {};
+    return product ? (
+        <Row>
+            <Col span={12}>
+            <img src={product.image} alt='product'/>
+            </Col>
+            <Col span={12}>
+            <p>{product.title}</p>
+            <p>{product.description}</p>
+            <p>{product.price}</p>
+            <Button type='primary'>Add to Cart</Button>
+            </Col>
+        </Row>
+    ) : " No product found";
 }
 
 export default ProductDetail
